793fb237d1e5f4ad86af3527675106663f1d08fb
Terry <terrykuo501@gmail.com>
2021-01-07 10:31:07 +0800

    Initial Commit

---
+++ TodoeyWithCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoeyWithCoreData
+//
+//  Created by Terry Kuo on 2021/1/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
